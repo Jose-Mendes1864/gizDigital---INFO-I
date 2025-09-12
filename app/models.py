76f2d33ec7c4588f5d9cfb54e7817dd87a044c1f
Jose <jmendes0501@gmail.com>
2025-09-12 19:35:23 -0300

    tentando ajeitaar

--- app/models.py
+++ app/models.py
@@ -1,3 +1,46 @@
 from django.db import models
+from autenticacao.models import Usuario
+from enum import Enum
 
 # Create your models here.
+usuario_padrao = Usuario.objects.get_or_create(username='deletado')[0]
+class StatusEnum(Enum):
+    RESOLVIDO = 'Resolvido'
+    EM_ANDAMENTO = 'Em andamento'
+    NAO_ANALISADO = 'Não analisado'
+    
+class Etiqueta(models.Model):
+    corRGB = models.CharField(max_length=13)
+    nome = models.CharField(max_length=200)
+class Comunidade(models.Model):
+    nome = models.CharField(max_length=60 )
+    descricao = models.TextField(verbose_name='descrições')
+    etiquetas = models.ManyToManyField(Etiqueta,related_name='comunidades')
+
+    def __str__(self):
+        return f'Comunidade - {self.nome}'
+
+
+class Post(models.Model):
+    comunidade = models.ForeignKey(Comunidade,on_delete=models.CASCADE)
+    usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+    descricao_post = models.CharField(max_length=400)
+    foto = models.ImageField(upload_to=f'usuario/posts', null=True, blank=True)
+    #preciso aqui identific ra imagme de quem é no banco de dados ex.: post1jose.png
+    curtidas = models.IntegerField(default=0)
+    data_criacao = models.DateTimeField()
+class Comentario(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    usuario = models.ForeignKey(Usuario, on_delete=models.SET_DEFAULT, default=usuario_padrao.id)
+    criacao = models.DateTimeField()
+    conteudo = models.TextField()
+class MotivoSuporte(models.Model):
+    motivo = models.CharField(max_length=60)
+    def __str__(self):
+        return self.motivo
+    
+class RequisicaoSuporte(models.Model):
+    motivo = models.ForeignKey(MotivoSuporte, on_delete=models.SET_NULL)
+    status = models.CharField(max_length=20, choices= [(tag.name, tag.value) for tag in StatusEnum])
+    descricao = models.TextField()
+    foto = models.FileField(upload_to='usuario/requisicao', null=True, blank=True)
